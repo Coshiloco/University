8a1665687bafb3eca0aa8862a81bd01db50e22df
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-06-27 19:00:19 +0200

    Controller De nuestro Modelo Usuarios

--- UiniversityApiBackend/Controllers/WeatherForecastController.cs
+++ UiniversityApiBackend/Controllers/WeatherForecastController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace UiniversityApiBackend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]")] /*COn route esta accediendo al 
+                             servidor nuestro de localhost localhost:7190/WeatherForecast*/
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -18,7 +19,11 @@ namespace UiniversityApiBackend.Controllers
             _logger = logger;
         }
 
+        //MEthod HTTTP de tipo get
         [HttpGet(Name = "GetWeatherForecast")]
+        /*Cuando llamamos al GetWeatherForecast 
+         lo que hacemos es que se ejecute esta funcion que 
+        devuelve un enumerable*/
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
