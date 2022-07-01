a8319b21f684f0921049c9d060aa4b93751d8f21
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-01 21:19:14 +0200

    ManejarCodigo

--- LinqSnippets/Snippets.cs
+++ LinqSnippets/Snippets.cs
@@ -9,7 +9,7 @@ namespace LinqSnippets
     public class Snippets
     {
         //Hacemos la funcion basica para buscar cosas
-       static public void BasicLinQ()
+        static public void BasicLinQ()
         {
             string[] cars =
             {
@@ -23,22 +23,22 @@ namespace LinqSnippets
             };
 
             //1. SELECT * of cars para hacer una consulta de todos los datos de todos los coches (SELECT ALL CARS)
-            
+
             var carlist = from car in cars select car;
-            
+
             // Arriba estamos utilizado una setencia SQL pero al Reves empezando por el from in select
-            
+
             foreach (var car in carlist)
             {
                 Console.WriteLine(car);
             }
 
             //2. SELECT WHERE es decir un select con condicionales como pro ejemplo que solo queremos los cohes que tengan Audi
-            
+
             var audioList = from car in cars where car.Contains("Audi") select car;
-            
+
             // Aqui solo tenemos la lista de los coches que tienen Audi
-            
+
             foreach (var audi in audioList)
             {
                 Console.WriteLine(audi);
@@ -53,14 +53,14 @@ namespace LinqSnippets
         static public void LinqNumbers()
         {
             //Vamos a trabajar ahora ocn listas de nuemros
-            
-            List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
             /*Las operaciones que vamos ha hacer son 
              MUltiplicar cada numero por tres | Each Number multiplied by 3
             Obtener todos los nuemros menos el 9 | take all numbers but 9
             Ordenar todos los numeros de forma ascendente | ORder numbers by ascending value*/
-            
+
             var processedNumberList = numbers
                 .Select(num => num * 3) // Al seleccionarlos los multiplicamos por 3 directamente
                 .Where(num => num != 9) // all but 9 | cojemos todos menos el 9 es decir los que sean distintos de 9
@@ -82,50 +82,50 @@ namespace LinqSnippets
             };
 
             //1. Search first of all elements encontrar el primero de los elementos
-            
+
             var first = textList.First();
-            
+
             //2. First element that is "c" | Coger el primer elemento que contenga al letra c
-            
+
             var FirstCLetterInTextList = textList.First(text => text.Equals("c"));
-            
+
             // CUando ponemos el => estamos diciendo que e suna funcion anonima es
-            
+
             // COmo un callback en JavaScript o TypeScript
-            
+
             /*Entonces lo que le estamos diciendo es una vez encuentres el primero
              me pasas la variable me la guardas en la variabel text dentro de los paraentesis
             y ejecuto un callback o segunda funcion que me compare
             si esa letra alamacenada en la variabel text sea igual a la letra c*/
-            
+
             //3.  First element that contains "j" | es decir el primer elemento que contenga la palabra j
-            
+
             var FirstJLetterInTextList = textList.First(text => text.Contains("j"));
-            
+
             //4. First element that contains  Z or default value | Priemr elemento que contenga la Z o valor por defecto
-            
+
             var FirstZOrDefault = textList.FirstOrDefault(text => text.Contains("z")); // Default " or first element that Contains "Z
-            
+
             // Si no encuentra la Z lo que te devulve es una cadena de texto vacia
-            
+
             //5. Last element that contains  Z or default value | Ultimo elemento que contenga la Z o valor por defecto
-            
+
             var LastZOrDefault = textList.LastOrDefault(text => text.Contains("z")); // Default " or last element that Contains "Z"
-            
+
             // 6. SIngle Values or Valores Unicos
-            
+
             var uniqueTexts = textList.Single();
-            
+
             var uniqueOrDefaultTexts = textList.SingleOrDefault();
 
             // Working this metods at the top with number trabajamos con los metodos pero ahora con numeros en vez
             // De con cadenas
 
             int[] evenNumbers = { 0, 2, 4, 6, 8 };
-            int[] otherNumebrs = { 0, 2, 6};
+            int[] otherNumebrs = { 0, 2, 6 };
 
             // Queremos obtener los nuemros que  no estan repetitdos
-            
+
             var noRepetNumbers = evenNumbers.Except(otherNumebrs); // Return {4, 8}
 
         }
@@ -237,8 +237,8 @@ namespace LinqSnippets
 
             // All enterprises at least has employees with at least 1000€ of salary
 
-            bool hasEmployeeWithSalaryMoreThan1000 = 
-                enterprises.Any(enterprise => 
+            bool hasEmployeeWithSalaryMoreThan1000 =
+                enterprises.Any(enterprise =>
                     enterprise.Employees.Any(employee => employee.Salary >= 1000));
 
         }
@@ -368,6 +368,191 @@ namespace LinqSnippets
             var TakeLastTwoValues = myList.TakeLast(2); // {9,10} == output
 
             var TakeWhileSmallerThan4 = myList.TakeWhile(nubersList => nubersList < 4); // {1,2,3} == output
+        }
+
+        // Paging es deicr que para luego posteriormente con el front hace una pequeña tabla que se pagina de 10 en 10 o lo que sea
+
+        // With Skip and Take dentro de una misma linea
+
+        static public IEnumerable<T> GetPage<T>(IEnumerable<T> collection, int pageNumber, int resultsPerPage)
+        {
+
+            /*Es decir Imaginemos que nos dice el page number 2 pues lo restamos 
+             y lo multplicamos por el intervalo de paginacion que nosotros hemos decidido*/
+
+            int startIndex = (pageNumber - 1) * resultsPerPage;
+
+            /*Luego Hacemos el SKIP del startIndex porque no nos interesa
+             y cogemos en la coleccion el intervalod e paginacion que nosotros le hemos marcado*/
+
+            return collection.Skip(startIndex).Take(resultsPerPage);
+        }
+
+        static public void LinqVariables()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var aboveAverage = from number in numbers
+                               let average = numbers.Average()
+                               let nSquare = Math.Pow(number, 2)
+                               where nSquare > average
+                               select number;
+
+            /*Las variables de tipo let que se encuentran dentro de esa consulta 
+             solo viven en la consulta por tanto se podrian considerar variables de ambito local
+            la spodemos hacer en toda la consulta por lo tanto lo que estamos haciendo
+            es recorrernos la lista de numeros almacenar en una variable local de la consulta con la palabra
+            reservada de tipo let la media o average
+            y con otra variable lo que estamos haciendo es guardar el cuadrado del numero 
+            de la lista que estamos recorriendo o seleccionando imitando una consulta 
+            sql con NSquare 
+            luego con el where establcecemos la condicion como si de 
+            sql se tratase y seleccionamos con el select aquellos que su raiz sea mayor que la media*/
+
+            // Tambien podemos imprimir la media
+
+            Console.WriteLine("Average {0} : ", numbers.Average());
+
+            // POdemos reocrrenos los valores devueltos por la consulta SQL que hemos hecho arriba con la variable aboveAverage
+
+            foreach (int number in aboveAverage)
+            {
+                Console.WriteLine("Query: Number {0} Square {1}", number, Math.Pow(number, 2));
+            }
+
+        }
+
+        // ZIP
+
+        static public void ZipLinq()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            string[] stringNumbers = { "one", "two", "three", "four", "five" };
+
+            IEnumerable<string> zipNubers = numbers.Zip(stringNumbers, (number, word) => number + "=" + word);
+
+            /*OutPut de la impresion de arriba 
+             {"1=one", "2=two" ...}
+            Es decir que lo hacemos es guardar en una variable de tipo
+            coleccion la lista empaquetada 
+            Por tanto para que haya un Zip para que podamos ejecutar esa funcion tiene que tener las dos listas el mismo
+            numero de parametros
+            por tanto lo que hace el ZIP le pasamos
+            priemro la lista de los string porque va a ser de tipo string nuestra coleccion y luego recibe en un
+            callback o funcion de retorno los numeros de numbers que es la lista que ejecuta la funcion y la palabra
+            del primer parametro y el resultado de ese callback hace que nos genere la nueva coleccion
+            con el formato que tiene el callback ene l cuerpo de nuestra fufncion de callback*/
+        }
+
+        // Repeat & Range
+
+        static public void RepeatRangeLinq()
+        {
+            //Generate collection from 1 - 1000
+
+            IEnumerable<int> first1000 = Enumerable.Range(1, 1000); // Generate a inetegral squence of numbers first start number second count
+
+            var QueryLinQ = from number in first1000
+                            select number;
+
+            // Repeat a value N times N represent a number of time that we dont know at this moment
+
+            IEnumerable<string> fiveXs = Enumerable.Repeat("x", 5); // output XXXXX {X,X,X,X,X}
+
+            // First parametre represent the data to repetir
+            // Secind parameter represent the time that it doing the repetition
+
+
+        }
+
+        static public void studentsLinq()
+        {
+            // Creamos la lista de estudiantes para hacer las consultas avanzadas
+
+            var classrom = new[]
+            {
+                new Student
+                {
+                    Id = 1,
+                    Name = "Martin",
+                    Grade = 80,
+                    Certified = true
+                },
+                new Student
+                {
+                    Id = 2,
+                    Name = "Juan",
+                    Grade = 50,
+                    Certified = false
+                },
+                new Student
+                {
+                    Id = 3,
+                    Name = "Ana",
+                    Grade = 96,
+                    Certified = true
+                },
+                new Student
+                {
+                    Id = 4,
+                    Name = "Alvaro",
+                    Grade = 10,
+                    Certified = false
+                },
+                new Student
+                {
+                    Id = 5,
+                    Name = "Pedro",
+                    Grade = 50,
+                    Certified = true
+                }
+
+            };
+
+            // Obtener los estudiantes que estan certificados
+
+            var certifiedStudents = from student in classrom
+                                    where student.Certified == true
+                                    select student;
+            //No certificados
+
+            var notCertified = from student in classrom
+                               where student.Certified == false
+                               select student;
+
+            // alumnos aprobadoe sesta va a ser con el && para que la selct te coja el segundo parametro
+
+            var approvedStudentsNames = from student in classrom
+                                   where student.Grade >= 50 && student.Certified == true
+                                   select student.Name;
+        
+        }
+
+        // All
+
+        static public void AllLinq()
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4, 5 };
+
+            bool allAresmallerThan10 = numbers.All(x => x < 10); //output true
+
+            /*Lo que le estamos diciendo es guardar en una variable un booleano
+             que nos coge el ALL y el callback o funcion nos devulve cada elemento y 
+            hacemos que la condicion sea menor que 10*/
+
+            bool allAreBiggerOrEqualThan2 = numbers.All(x => x >= 2); // output false
+
+            var emptyList = new List<int>();
+
+            bool allNumbersAreGreaterThan0 = numbers.All(x => x >= 0) // true output
+        }
+
+        // Aggregate 
+
+        static public void aggregateQueries()
+        {
+
         }
     }
 }
